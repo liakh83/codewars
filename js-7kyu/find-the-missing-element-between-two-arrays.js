74a0761d768f1900b867916094dbd9c35a57cf9a
liakh83 <lahn4573@gmail.com>
2025-10-06 15:49:59 +0300

    add solution find-the-missing-element-between-two-arrays

---
+++ js-7kyu/find-the-missing-element-between-two-arrays.js
@@ -0,0 +1,27 @@
+/*
+url: https://www.codewars.com/kata/find-the-missing-element-between-two-arrays
+
+Description:
+Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, find the missing element.
+
+Please note, there may be duplicates in the arrays, so checking if a numerical value exists in one and not the other is not a valid solution.
+
+find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+The first array will always have at least one element.
+*/
+
+function findMissing(arr1, arr2) {
+  const array1 = arr1.sort((a,b) => (a-b))
+  const array2 = arr2.sort((a,b) => (a-b))
+  console.log(array1, array2 )
+  for (let i = 0; i <= arr1.length; i++) {
+
+    if(!(array1[i] === array2[i])) {
+      console.log(array1[i] !== array2[i])
+      return array1[i]
+    }
+  }
+}
+
+console.log(findMissing([0, 0, 0, 0, 0], [0, 0, 0, 0]));
