9ae644caf4909d624b7d479f4bebbe7ffc6f0ec1
liakh83 <lahn4573@gmail.com>
2025-10-14 11:10:19 +0300

    add solution rgb-to-hex-conversion

---
+++ js-5kyu/rgb-to-hex-conversion.js
@@ -0,0 +1,40 @@
+/*
+url : https://www.codewars.com/kata/rgb-to-hex-conversion
+
+Description:
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
+Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+Examples (input --> output):
+255, 255, 255 --> "FFFFFF"
+255, 255, 300 --> "FFFFFF"
+0, 0, 0       --> "000000"
+148, 0, 211   --> "9400D3"
+*/
+
+function rgb(r, g, b) {
+  const arr = [r, g, b];
+  const sortArr = arr.map((num) => {
+    if (num < 0) {
+      return 0;
+    } else if (num > 255) {
+      return 255;
+    }
+    return num;
+  });
+  // console.log(sortArr);
+
+  const hexColor = sortArr.map((color) => {
+    // console.log(color.toString(16).toUpperCase());
+    return color.toString(16).toUpperCase().padStart(2, 0);
+  });
+
+  // console.log(hexColor);
+  return hexColor.join("");
+}
+
+300, 255, 255, "FFFFFF";
+148, 0, 211, "9400D3";
+console.log(rgb(0, 0, -20)); //'000000'
